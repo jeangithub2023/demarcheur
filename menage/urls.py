3a02ec5ebd8f7dbff3f4e4f5ae4a5e33bca8bfe6
jk <jeantexte20@gmail.com>
2023-02-11 16:23:18 +0100

    premiere sauvegarde

--- menage/urls.py
+++ menage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from menage.views import contact, detail, index
+from menage.views import contact, detail, index, menage
 
 app_name='menage'
 
 urlpatterns = [
     path('', index, name='home'),
     path('<int:id>/detail', detail, name='detail'),
+    path('<int:id>/menage', menage, name='menage'),
     path('contact/', contact, name='contact'),
 ]
--- menage/views.py
+++ menage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from menage.models import Menus
+from menage.models import Menus, Services
 
 # Create your views here.
 
@@ -12,11 +12,22 @@ def index(request):
     return render(request,'menage/index.html', context)
 
 def detail(request, id):
-    service=get_object_or_404(Menus,id=id)
+    menu=get_object_or_404(Menus,id=id)
+    print(menu)
+    services=Services.objects.filter(menu=menu)
+    print(services)
     context={
-        "service": service
+        "menu": menu,
+        "services": services
     }
     return render(request,'menage/detail.html', context)
 
 def contact(request):
     return render(request, 'menage/contact.html')
+
+def menage(request,id):
+    service=get_object_or_404(Services,id=id)
+    context={
+        "service": service
+    }
+    return render(request,'menage/menage.html',context)
